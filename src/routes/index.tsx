01f753776083b9fbba99ad3b7abda96175b64b6f
franck-co <franck@pedagome.fr>
2024-07-03 12:08:45 +0000

    demo on index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,79 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {
+  type DocumentHead,
+  routeLoader$,
+  routeAction$,
+  zod$,
+  z,
+  Form,
+} from "@builder.io/qwik-city";
+import styles from "./todolist.module.css";
+import { title } from './demo.css';
 
-import Counter from "../components/starter/counter/counter";
-import Hero from "../components/starter/hero/hero";
-import Infobox from "../components/starter/infobox/infobox";
-import Starter from "../components/starter/next-steps/next-steps";
+interface ListItem {
+  text: string;
+}
+
+export const list: ListItem[] = [];
+
+export const useListLoader = routeLoader$(() => {
+  return list;
+});
+
+export const useAddToListAction = routeAction$(
+  (item) => {
+    list.push(item);
+    return {
+      success: true,
+    };
+  },
+  zod$({
+    text: z.string().trim().min(1),
+  }),
+);
 
 export default component$(() => {
+  const list = useListLoader();
+  const action = useAddToListAction();
+
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
+      <div class="container container-center">
+        <h1>
+          <span class={title}>TODO</span> List
+        </h1>
       </div>
 
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
+      <div role="presentation" class="ellipsis"></div>
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
+      <div class="container container-center">
+        {list.value.length === 0 ? (
+          <span class={styles.empty}>No items found</span>
+        ) : (
+          <ul class={styles.list}>
+            {list.value.map((item, index) => (
+              <li key={`items-${index}`}>{item.text}</li>
+            ))}
+          </ul>
+        )}
+      </div>
 
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.dev/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/QwikDev/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.dev/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
+      <div class="container container-center">
+        <Form action={action} spaReset>
+          <input type="text" name="text" required class={styles.input} />{" "}
+          <button type="submit" class="button-dark">
+            Add item
+          </button>
+        </Form>
+
+        <p class={styles.hint}>
+          PS: This little app works even when JavaScript is disabled.
+        </p>
       </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+  title: "Qwik Todo List",
 };
